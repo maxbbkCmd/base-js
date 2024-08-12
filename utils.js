8c015f7dc11e21cb004963281dc2e93c6e05ac5d
maxbbkCmd <maxbbkCmd@mail.ru>
2024-08-13 01:07:22 +0300

    Function calcSum

---
+++ utils.js
@@ -0,0 +1,13 @@
+/**
+ * @function calcSum 
+ * @param {number} num1
+ * @param {number} num2
+ * @throws {Error} Если аргумент не число
+ * @return (number) сумма чисел
+ */
+
+export  const calcSum = (num1, num2) => {
+  return num1 + num2;
+};
+
+ 
